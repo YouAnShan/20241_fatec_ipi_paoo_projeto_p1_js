b8b137c6f2afa6a6028afda6f82d4bd2fd5079c5
João Vitor Serra de Freitas <youanshan@gmail.com>
2024-04-11 13:53:57 -0300

    feat(p1): obtem previsao a partit de coords

--- projeto.js
+++ projeto.js
@@ -8,6 +8,8 @@
             - obtém as coordenadas latitude/longitude dessa cidade
 
     Mensagem do commit: feat(p1): obtem coords a partir de cidade
+
+    -- CONCLUÍDO 11/04/2024
     
     2. Consulta de condições atuais e função de latitude/longitude
     
@@ -25,24 +27,38 @@
 require('dotenv').config() 
 const axios = require('axios')
 
-const {APPID, URLBASE, Q} = process.env
-
+const {APPID, URLBASE, Q, URLBASE2, IDIOMA, UNITS} = process.env
 
 const url = `${URLBASE}?q=${Q}&appid=${APPID}`
 
+let LAT, LON;
 
 axios.get(url)
 .then(res => {
-    console.log('Latitude: ')
-    console.log(res.data[0].lat)
+    LAT = res.data[0].lat;
+    console.log('Latitude: ', LAT)
     return res
 })
 .then(res => {
-    console.log('Longitude: ')
-    console.log(res.data[0].lon)
+    LON = res.data[0].lon;
+    console.log('Longitude: ', LON)
+    const url2 = `${URLBASE2}?lat=${LAT}&lon=${LON}&appid=${APPID}&lang=${IDIOMA}&units=${UNITS}` // >> Tentei colocar fora mas ele foi compilado antes de obter a latitude e a longitude sendo executada imediatamente após a chamada da primeira URL.
+    axios.get(url2)
+    .then(res => {
+        console.log('Sensação Térmica:',res.data.main.feels_like,'ºC')
+        return res
+    })
+    .then(res => {
+        console.log('O tempo está',res.data.weather[0].description,'!')
+        return res
+    })
     return res
 })
-
-
+.catch(error => {
+    console.error('Erro:', error);
+});
 
 //data: [{name: 'London',local_names: [Object],lat: 51.5073219,lon: -0.1276474,country: 'GB',state: 'England'}]
+
+//data: {coord: { lon: -0.1276, lat: 51.5073 }, weather: [ [Object] ], base: 'stations', main: {temp: 18.61, feels_like: 18.41, temp_min: 16.19, temp_max: 19.51, pressure: 1028, humidity: 72}, visibility: 10000, wind: { speed: 3.09, deg: 210 }, clouds: { all: 20 }, dt: 1712852070, sys: {type: 2, id: 2075535, country: 'GB', sunrise: 1712812352, sunset: 1712861397}, timezone: 3600, id: 2643743, name: 'London', cod: 200}
+
